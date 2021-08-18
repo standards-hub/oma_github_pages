62ce252308888be56f55f46030b2d2525c180b05
ladyvargas <lady.vargas.santana@gmail.com>
2021-08-18 11:16:06 -0500

    Updates

--- _nuxt/static/1629302488/posts/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Foma_github_pages\u002F_nuxt\u002Fstatic\u002F1629302488",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts",config:{_app:{basePath:"\u002Foma_github_pages\u002F",assetsPath:"\u002Foma_github_pages\u002F_nuxt\u002F",cdnURL:a}}}}(null));
