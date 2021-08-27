dec11349428f8cebedb0e10d630c0abfcea627bd
LADY VARGAS <lady.vargas@pucese.edu.ec>
2021-08-27 12:53:26 -0500

    Updates

--- _nuxt/static/1630086461/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fcontact-us","\u002Fposts\u002Fmy-day","\u002Fabout","\u002F","\u002Finfo","\u002Fposts\u002Ffirst-post","\u002Fnew","\u002Fposts","\u002Fnew2","\u002Ftable"]})
---
+++ _nuxt/static/1630086774/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fposts\u002Ffirst-post","\u002Fabout","\u002F","\u002Fcontact-us","\u002Finfo","\u002Fposts\u002Fmy-day","\u002Fnew2","\u002Ftable","\u002Fnew","\u002Fposts"]})
