6dc43b5111f85f350eadd730f28673cec566b05d
ladyvargas <lady.vargas.santana@gmail.com>
2021-08-30 09:02:54 -0500

    Updates

---
+++ _nuxt/static/1628703733/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fcontact-us","\u002Fposts\u002Ffirst-post","\u002F","\u002Fabout","\u002Fposts\u002Fmy-day","\u002Fposts","\u002Ftable"]})
--- _nuxt/static/1630086774/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fposts\u002Ffirst-post","\u002Fabout","\u002F","\u002Fcontact-us","\u002Finfo","\u002Fposts\u002Fmy-day","\u002Fnew2","\u002Ftable","\u002Fnew","\u002Fposts"]})
